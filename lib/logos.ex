a119cb43b6f0bd3e14a0dd0c85080323c63a3f37
thanos <thanosv@gmail.com>
2025-11-04 20:25:07 -0500

    added project structure

---
+++ lib/logos.ex
@@ -0,0 +1,18 @@
+defmodule Logos do
+  @moduledoc """
+  Documentation for `Logos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Logos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
