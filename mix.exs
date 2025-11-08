0388f29869ebc972a29efa604d9fc6b0e8d64027
thanos <thanosv@gmail.com>
2025-11-08 00:39:24 -0500

    update mix package details

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Logos.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +24,17 @@ defmodule Logos.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Thanos Vassilakis"],
+      licenses: ["MPL-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/thanos/logos",
+        "Documentation" => "https://hexdocs.pm/logos"
+      }
     ]
   end
 end
